6f659a3f8837ebe3b613f4bc0b339d1b7b6dcef5
maximumpower55 <62370320+maximumpower55@users.noreply.github.com>
2021-03-02 20:19:18 -0800

    big refacter

---
+++ src/main/java/me/maximumpower55/enhancement/Enhancement.java
@@ -0,0 +1,10 @@
+package me.maximumpower55.enhancement;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Enhancement implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
